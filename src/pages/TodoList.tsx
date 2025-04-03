15051ad55f0d136b64c5a5ad67d52e3c1d9c4a36
tombannister01 <tombannister2463@gmail.com>
2025-04-03 10:32:36 +0100

    adding basic todoList functionality

--- src/pages/TodoList.tsx
+++ src/pages/TodoList.tsx
@@ -1,5 +1,34 @@
+import { useState } from "react";
+
 const TodoList: React.FC = () => {
-  return <div>Todo List</div>
+  const [list, setList] = useState<string[]>([]);
+  const [input, setInput] = useState<string>("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (input.trim()) {
+      setList(list.concat(input));
+      setInput("");
+    }
+
+
+  }
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value)
+  }
+
+
+  return (
+    <>
+      <ul>
+        {list.map((listItem: string, index: number) => <li key={index}>{listItem}</li>)}
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <input name="input" onChange={handleInput} />
+      </form>
+    </>
+  )
 }
 
 export default TodoList;
