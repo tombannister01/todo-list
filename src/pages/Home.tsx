5fc1bc93b0edd7eab74e15d325098169853dc225
tombannister01 <tombannister2463@gmail.com>
2025-04-04 20:39:30 +0100

    Clear list item functionality

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,20 @@
+import { useNavigate } from "react-router-dom";
+
+
+
+
 const Home: React.FC = () => {
-  return <div>Home</div>
+
+  const navigate = useNavigate()
+
+  return (
+    <>
+      <button onClick={() => navigate('/todo-list')}>Todo-list</button>
+      <div>Home</div>
+
+    </>
+  )
+
 }
 
 export default Home;
--- src/pages/TodoList.tsx
+++ src/pages/TodoList.tsx
@@ -1,27 +1,57 @@
 import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+
+interface IListItem {
+  listItem: string;
+  done: boolean;
+  id?: string;
+}
 
 const TodoList: React.FC = () => {
-  const [list, setList] = useState<string[]>([]);
+
+
+
+  const [list, setList] = useState<IListItem[]>([]);
   const [input, setInput] = useState<string>("");
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     if (input.trim()) {
-      setList(list.concat(input));
+      setList([...list, { listItem: input, done: false, id: uuidv4() }]);
       setInput("");
     }
 
+  }
+
+  const toggleDone = (id: string | undefined) => {
+    const updatedList: IListItem[] = list.map((item) => {
+      if (item.id === id) {
+        return { listItem: item.listItem, done: !item.done };
+      }
+      return item;
+    })
+
+    setList(updatedList);
   }
 
 
   return (
     <>
       <ul>
-        {list.map((listItem: string, index: number) => <li key={index}>{listItem}</li>)}
+        {list.map((item: IListItem) => {
+          return <>
+            <li style={{ textDecoration: item.done ? 'line-through' : 'none' }} key={item.id}>
+              {item.listItem}
+              {!item.done && <button onClick={() => toggleDone(item.id)}>Clear</button>}
+            </li>
+          </>
+        })
+        }
       </ul>
       <form onSubmit={handleSubmit}>
         <input name="input" onChange={(e) => setInput(e.target.value)} />
+
       </form>
     </>
   )
